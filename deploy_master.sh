f42faa9b96dcaa32321ac5f8628d9baa389f62df
gurezo <akihiko.kigure@gmail.com>
2017-02-08 17:23:18 +0900

    Circle CI テスト

---
+++ deploy_master.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -ex
+
+echo "install angular-cli"
+npm install -g angular-cli
+
+echo "ng build"
+ng build
+
+echo "azure ftp deploy"
+lftp -u AzureCIExample\\gurezo,yamahaow01 -e "mirror -Re dist/. /site/wwwroot/." ftp://waws-prod-sn1-089.ftp.azurewebsites.windows.net
