7e4ea58b7652cad571ac69ebe18a681a68f8eb64
gurezo <akihiko.kigure@gmail.com>
2017-02-09 13:47:11 +0900

    test 13:47

--- deploy_master.sh
+++ deploy_master.sh
@@ -9,4 +9,4 @@ echo "ng build...."
 ng build
 
 echo "azure ftp deploy...."
-lftp -u AzureCIExample\\gurezo,yamahaow01 -e "mirror -Re dist/. /site/wwwroot/." ftp://waws-prod-sn1-089.ftp.azurewebsites.windows.net
+lftp -u AzureCIExample\\gurezo,yamahaow01 -e "mirror -Rev dist/. /site/wwwroot/." ftp://waws-prod-sn1-089.ftp.azurewebsites.windows.net
