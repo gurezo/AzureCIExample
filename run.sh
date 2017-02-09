b3726095d3c18119e7f3a59243f488233bef29f4
gurezo <akihiko.kigure@gmail.com>
2017-02-09 09:54:51 +0900

    step-azure-ftp-deploy

---
+++ run.sh
@@ -0,0 +1,16 @@
+#check for curl
+#check if password is filled
+#check if publish url starts with ftp
+#check if publish url endswith wwwroot
+
+FTP_URL=$WERCKER_AZURE_FTP_DEPLOY_PUBLISH_URL
+FTP_PASSWORD=$WERCKER_AZURE_FTP_DEPLOY_PASSWORD
+FTP_USERNAME=$WERCKER_AZURE_FTP_DEPLOY_USERNAME
+
+echo "Test connection"
+
+echo "curl -u $FTP_USERNAME:FTP_PASSWORD $FTP_URL/"
+curl -u $FTP_USERNAME:$FTP_PASSWORD $FTP_URL/
+
+echo "find . -type f -exec curl -u $FTP_USERNAME:FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;"
+find . -type f -exec curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;
