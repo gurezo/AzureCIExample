18ec50024368cc432d02435aea279918973a3283
gurezo <akihiko.kigure@gmail.com>
2017-02-09 11:14:04 +0900

    テスト

---
+++ file_clear.sh
@@ -0,0 +1,10 @@
+rm -rf favicon.ico
+rm -rf index.html
+rm -rf vendor.bundle.js
+rm -rf vendor.bundle.map
+rm -rf styles.bundle.map
+rm -rf styles.bundle.css
+rm -rf main.bundle.map
+rm -rf main.bundle.js
+rm -rf inline.bundle.map
+rm -rf inline.bundle.js
